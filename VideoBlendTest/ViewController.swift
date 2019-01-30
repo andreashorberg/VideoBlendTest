c775f1bc4df71b1df55bf73ede450746bbee5f58
Andreas Hörberg <andreas.horberg@daresay.co>
2019-01-30 16:56:14 +0100

    Initial Commit

---
+++ VideoBlendTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoBlendTest
+//
+//  Created by Andreas Hörberg on 2019-01-30.
+//  Copyright © 2019 Andreas Hörberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
